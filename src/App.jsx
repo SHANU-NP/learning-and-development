23cf31a4203eba75071735a46fe137752984ff07
shanunp <shanunp2016@gmail.com>
2025-06-22 12:47:54 +0530

    feat: useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Search from './component/Search'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      hello world
+   
+   <Search></Search>
       
     </div>
   )
