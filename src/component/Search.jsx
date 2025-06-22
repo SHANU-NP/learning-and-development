53bb2509770d933abc04b2b8ae9214c185d56459
shanunp <shanunp2016@gmail.com>
2025-06-22 13:25:20 +0530

    feat: use effect hook calling external API

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import { useState } from "react"
 
 export default function Search(){
 
-    const [query,setQuery] = useState("");
+    const [query,setQuery] = useState("pizza");
+
+    const URL = "https://api.spoonacular.com/recipes/complexSearch";
+    const API_KEY = "27eef40f344c4d2faa776299fc241a74";
+
+    //use effect
+    useEffect(() => {
+        async function fetchFood(){
+            const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
+            const data = await res.json();
+            console.log(data.results);
+        }
+      //  fetchFood()
+
+    },[query])
+
     return (
         <div>
 
